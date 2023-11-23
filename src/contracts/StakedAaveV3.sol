a61440b2c4d8098b6d79d1037d4c5471e40303c6
sendra <polsendra@gmail.com>
2023-11-23 17:18:33 +0100

    fix: correct logic

--- src/contracts/StakedAaveV3.sol
+++ src/contracts/StakedAaveV3.sol
@@ -68,40 +68,6 @@ contract StakedAaveV3 is StakedTokenV3, IStakedAaveV3 {
     return _claimRewardsAndStakeOnBehalf(from, to, amount);
   }
 
-  /// @notice Assembly implementation of the gas limited call to avoid return gas bomb,
-  /// moreover call to a destructed plugin would also revert even inside try-catch block in Solidity 0.8.17
-  function _updateDiscountDistribution(
-    address ghoDebtToken,
-    address from,
-    address to,
-    uint256 fromBalanceBefore,
-    uint256 toBalanceBefore,
-    uint256 amount
-  ) internal {
-    bytes4 selector = IGhoVariableDebtTokenTransferHook
-      .updateDiscountDistribution
-      .selector;
-    uint256 gasLimit = 200_000; // TODO: put correct gas limit
-    assembly ('memory-safe') {
-      // solhint-disable-line no-inline-assembly
-      let ptr := mload(0x40)
-      mstore(ptr, selector)
-      mstore(add(ptr, 0x04), from)
-      mstore(add(ptr, 0x24), to)
-      mstore(add(ptr, 0x44), fromBalanceBefore)
-      mstore(add(ptr, 0x64), toBalanceBefore)
-      mstore(add(ptr, 0x84), amount)
-
-      let gasLeft := gas()
-      if iszero(call(gasLimit, ghoDebtToken, 0, ptr, 0xA4, 0, 0)) {
-        if lt(div(mul(gasLeft, 63), 64), gasLimit) {
-          returndatacopy(ptr, 0, returndatasize())
-          revert(ptr, returndatasize())
-        }
-      }
-    }
-  }
-
   /**
    * - On _transfer, it updates discount, rewards & delegation for both "from" and "to"
    * - On _mint, only for _to
@@ -127,18 +93,32 @@ contract StakedAaveV3 is StakedTokenV3, IStakedAaveV3 {
       amount
     );
 
-    IGhoVariableDebtTokenTransferHook cachedGhoDebtToken = ghoDebtToken;
-    if (address(cachedGhoDebtToken) != address(0)) {
-      try
-        _updateDiscountDistribution(
-          cachedGhoDebtToken,
-          from,
-          to,
-          fromBalanceBefore,
-          toBalanceBefore,
-          amount
-        )
-      {} catch (bytes memory) {}
+    address cachedGhoDebtToken = address(ghoDebtToken);
+    if (cachedGhoDebtToken != address(0)) {
+      /// @notice Assembly implementation of the gas limited call to avoid return gas bomb,
+      /// moreover call to a destructed plugin would also revert even inside try-catch block in Solidity 0.8.17
+      bytes4 selector = IGhoVariableDebtTokenTransferHook
+        .updateDiscountDistribution
+        .selector;
+      uint256 gasLimit = 12_101;
+      assembly ('memory-safe') {
+        // solhint-disable-line no-inline-assembly
+        let ptr := mload(0x40)
+        mstore(ptr, selector)
+        mstore(add(ptr, 0x04), from)
+        mstore(add(ptr, 0x24), to)
+        mstore(add(ptr, 0x44), fromBalanceBefore)
+        mstore(add(ptr, 0x64), toBalanceBefore)
+        mstore(add(ptr, 0x84), amount)
+
+        let gasLeft := gas()
+        if iszero(call(gasLimit, cachedGhoDebtToken, 0, ptr, 0xA4, 0, 0)) {
+          if lt(div(mul(gasLeft, 63), 64), gasLimit) {
+            returndatacopy(ptr, 0, returndatasize())
+            revert(ptr, returndatasize())
+          }
+        }
+      }
     }
   }
 }
